95a3c46cabc3c17284f721450f1b8d55ac20b745
Andrii Sereda <sa0959328930@gmail.com>
2024-05-01 16:28:43 +0300

    refactoring

--- src/main/resources/db/changelog/0.0.1/v1-tables.sql
+++ src/main/resources/db/changelog/0.0.1/v1-tables.sql
@@ -3,7 +3,7 @@
 --changeset Andrii Sereda:v1-1 runOnChange:true
 CREATE TABLE companies (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
     country VARCHAR(255) NOT NULL,
     working_cars_amount INT CHECK (working_cars_amount >= 0)
 );
@@ -11,10 +11,11 @@ CREATE TABLE companies (
 CREATE TABLE taxi_drivers (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
     company_id INTEGER NOT NULL,
     age INT CHECK (age >= 18 AND age <= 100),
     driving_experience INT CHECK (driving_experience >= 0 AND driving_experience <= 90),
     salary BIGINT CHECK (salary >= 0 AND salary <= 10000000),
     cars VARCHAR(255) NOT NULL,
-    FOREIGN KEY (company_id) REFERENCES companies(id)
+    FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE SET NULL
 );
